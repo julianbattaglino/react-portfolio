022c3261c6db2154f820fae0f9e2a11eeb1c449f
Julian Battaglino <julianbattaglino@gmail.com>
2022-09-10 16:04:22 -0300

    AboutMe component

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+import "./AboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <div className="intro-container">
+      <h1 className="main-title">Sobre Mi</h1>
+    </div>
+  );
+}
+
+export default AboutMe;
