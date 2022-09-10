918f47b36eb02d3865f4621d2e134aeea83293f3
Julian Battaglino <julianbattaglino@gmail.com>
2022-09-10 16:05:09 -0300

    Projects component

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import "./Projects.css";
+
+const Projects = () => {
+  return (
+    <div className="intro-container">
+      <h1 className="main-title">Proyectos</h1>
+    </div>
+  );
+}
+
+export default Projects;
