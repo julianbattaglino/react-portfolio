2d04f6035bffe33819daa3a6719f4c406f2278ca
Julian Battaglino <julianbattaglino@gmail.com>
2022-09-15 14:53:58 -0300

    email.js dependency added & test submit email

---
+++ src/components/ContactUs/ContactUs.js
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_f3w8m49', 'template_5ejx1db', form.current, 'QQ1yOj151Ud7RZlrY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <button onClick={sendEmail} type="submit">Send</button>
+    </form>
+  );
+};
+
+export default ContactUs;
