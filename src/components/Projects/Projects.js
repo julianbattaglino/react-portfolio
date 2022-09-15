e1159733ecbe261a117f9a4ecaf9627a730b990f
Julian Battaglino <julianbattaglino@gmail.com>
2022-09-15 14:53:22 -0300

    Add useState & useEffect hook for render projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,160 +1,67 @@
 import "./Projects.css";
-import { Box, Image } from '@chakra-ui/react'
+import { Box, Image, Link } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import ProjectsMock from "../ProjectsMock/ProjectsMock";
+import React, { useState, useEffect } from "react";
 
 
 const Projects = () => {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    setProjects(ProjectsMock);
+  }, []);
+
+
+  //map projects to display on page 
+  const projecstGrid = projects.map((project) => {
+    console.log(project);
+
+    return (
+      <>
+
+        <Box className="card-item" key={project.id} maxW='100%' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+
+          <Image src={project.image} alt="" />
+
+          <Box p='6'>
+            <Box display='flex' alignItems='baseline'>
+              <Box color='gray.500' fontWeight='semibold' letterSpacing='wide' fontSize='xs' textTransform='uppercase'>
+                {project.category}
+              </Box>
+            </Box>
+
+            <Box className="project-title" mt='1' fontWeight='semibold' as='h4' lineHeight='tight' noOfLines={1}>
+              {project.title}
+            </Box>
+
+            <Box>
+              <Box as='span' color='gray.600' fontSize='sm'>
+                {project.location}
+              </Box>
+            </Box>
+
+            <Box>
+              <Link href={project.deployed} isExternal>
+                Project deploy <ExternalLinkIcon mx='2px' />
+              </Link>            </Box>
+
+
+          </Box>
+        </Box>
+      </>
+
+    );
+  });
+
   return (
+    <div className="intro-container">
+      <h1 className="main-title"><span class="underline-nice">Projects</span></h1>
+      <div className="grid-container">{projecstGrid}</div>
 
-    <>
-      <div className="intro-container">
-        <h1 className="main-title"><span class="underline-nice">Projects</span></h1>
-      </div>
-
-      <div className="grid-container">
-        <Box className="card-item" maxW='100%' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-          <Image src="/img/portfolio/arcos4259.webp" alt="" />
-
-          <Box p='6'>
-            <Box display='flex' alignItems='baseline'>
-              <Box
-                color='gray.500'
-                fontWeight='semibold'
-                letterSpacing='wide'
-                fontSize='xs'
-                textTransform='uppercase'
-
-              >
-                Desarrollos Inmobiliarios
-              </Box>
-            </Box>
-
-            <Box className="project-title"
-              mt='1'
-              fontWeight='semibold'
-              as='h4'
-              lineHeight='tight'
-              noOfLines={1}
-            >
-              Arcos 4259
-            </Box>
-
-            <Box>
-              <Box as='span' color='gray.600' fontSize='sm'>
-                Nuñez, Buenos Aires, Argentina.
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-
-        <Box className="card-item" maxW='100%' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-          <Image src="/img/portfolio/haemprendimientos.webp" alt="" />
-
-          <Box p='6'>
-            <Box display='flex' alignItems='baseline'>
-              <Box
-                color='gray.500'
-                fontWeight='semibold'
-                letterSpacing='wide'
-                fontSize='xs'
-                textTransform='uppercase'
-              >
-                Desarrollos Inmobiliarios
-              </Box>
-            </Box>
-
-            <Box className="project-title"
-              mt='1'
-              fontWeight='semibold'
-              as='h4'
-              lineHeight='tight'
-              noOfLines={1}
-            >
-              HA Emprendimientos
-            </Box>
-
-            <Box>
-              <Box as='span' color='gray.600' fontSize='sm'>
-                Beccar Buenos Aires, Argentina.
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-
-
-        <Box className="card-item" maxW='100%' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-          <Image src="/img/portfolio/sensemanantiales.webp" alt="" />
-
-          <Box p='6'>
-            <Box display='flex' alignItems='baseline'>
-              <Box
-                color='gray.500'
-                fontWeight='semibold'
-                letterSpacing='wide'
-                fontSize='xs'
-                textTransform='uppercase'
-              >
-                Desarrollos Inmobiliarios.
-              </Box>
-            </Box>
-
-            <Box className="project-title"
-              mt='1'
-              fontWeight='semibold'
-              as='h4'
-              lineHeight='tight'
-              noOfLines={1}
-            >
-              Sense Manantiales
-            </Box>
-
-            <Box>
-              <Box as='span' color='gray.600' fontSize='sm'>
-                Maldonado, Uruguay.
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-
-        <Box className="card-item" maxW='100%' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-          <Image src="/img/portfolio/senselibertador.webp" alt="" />
-
-          <Box p='6'>
-            <Box display='flex' alignItems='baseline'>
-              <Box
-                color='gray.500'
-                fontWeight='semibold'
-                letterSpacing='wide'
-                fontSize='xs'
-                textTransform='uppercase'
-              >
-                Desarrollos Inmobiliarios.
-              </Box>
-            </Box>
-
-            <Box className="project-title"
-              mt='1'
-              fontWeight='semibold'
-              as='h4'
-              lineHeight='tight'
-              noOfLines={1}
-            >
-              Sense Libertador
-            </Box>
-
-            <Box>
-              <Box as='span' color='gray.600' fontSize='sm'>
-                Accasuso, Buenos Aires, Argentina.
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-      </div>
-
-
-
-    </>
-
+    </div>
   );
-}
+};
 
 export default Projects;
