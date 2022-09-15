369768b4b451a240d7275a659ea590015217e138
Julian Battaglino <julianbattaglino@gmail.com>
2022-09-15 14:54:16 -0300

    update app.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,8 @@ import "./Home.css";
 import { SiGithub, SiLinkedin } from "react-icons/si";
 import { ImFilePdf } from "react-icons/im";
 import Pdf from '../../cv-julian-battaglino.pdf';
+import ContactUs from "../ContactUs/ContactUs";
+
 
 const Home = () => {
 
@@ -17,16 +19,17 @@ const Home = () => {
                 <h1 className="main-title"><span class="underline-nice">Julian Battaglino</span></h1>
                 <h2 className="subtitle">Front End Developer</h2>
                 <div className="icon-container">
-                    <a href="https://github.com/julianbattaglino" target="_blank" className="icon-link">
+                    <a href="https://github.com/julianbattaglino" target="_blank" rel="noreferrer" className="icon-link">
                         <SiGithub size={30} className="icon" />
                     </a>
-                    <a href="https://github.com/julianbattaglino" target="_blank" className="icon-link">
+                    <a href="https://github.com/julianbattaglino" target="_blank" rel="noreferrer" className="icon-link">
                         <SiLinkedin size={30} className="icon" />
                     </a>
                     <ImFilePdf onClick={onResumeClick} size={30} className="icon" />
                 </div>
                 <img className="profile-img" src="/img/profile-pic.png" alt="profile-img" />
             </div>
+            {/* <ContactUs /> */}
         </>
     );
 }
