ac9d4f5b9726921511b209a4cb4508cc48f65390
Julian Battaglino <julianbattaglino@gmail.com>
2022-09-15 14:52:21 -0300

    Portfolio projects mock js

---
+++ src/components/ProjectsMock/ProjectsMock.js
@@ -0,0 +1,53 @@
+
+const ProjectsMock = [ 
+    {
+        id: 1,
+        title: 'Learn idiomas',
+        category: 'Education',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.',
+        location: 'Toulon, Hyères, France',
+        image: '/img/portfolio/learn-idiomas.webp',
+        deployed: 'https://julianbattaglino.github.io/LearnIdiomas/', 
+    },
+    {
+        id: 1,
+        title: 'Arcos 4259',
+        category: 'Desarrollos Inmobiliarios',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.',
+        location: 'Nuñez, Buenos Aires, Argentina.',
+        image: '/img/portfolio/arcos4259.webp',
+        deployed: 'https://julianbattaglino.github.io/Arcos-4259---Revision-Final/', 
+    },
+    {
+        id: 2,
+        title: 'HA Emprendimientos',
+        category: 'Desarrollos Inmobiliarios',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.',
+        location: 'Beccar Buenos Aires, Argentina.',
+        image: '/img/portfolio/haemprendimientos.webp',
+        deployed: 'https://julianbattaglino.github.io/ha-web/', 
+    },
+    {
+        id: 3,
+        title: 'Sense Manantiales',
+        category: 'Desarrollos Inmobiliarios',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.',
+        location: 'Manantiales, Maldonado, Uruguay.',
+        image: '/img/portfolio/sensemanantiales.webp',
+        deployed: 'https://julianbattaglino.github.io/sense-manantiales/', 
+    },
+    {
+        id: 4,
+        title: 'Sense Libertador',
+        category: 'Desarrollos Inmobiliarios',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Donec auctor, nisl eget aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.',
+        location: 'Accasuso, Buenos Aires, Argentina.',
+        image: '/img/portfolio/senselibertador.webp',
+        deployed: 'https://julianbattaglino.github.io/sense-libertador/', 
+    },
+
+];
+
+export default ProjectsMock;
+
+    
