35939fcd175625df520225c486aed6a4ec090e7b
Julian Battaglino <julianbattaglino@gmail.com>
2023-01-13 15:10:39 -0300

    home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,14 +30,20 @@ const Home = () => {
                 <img className="profile-img" src="/img/profile-pic.png" alt="profile-img" />
 
 
-                <h2 className="subtitle">Tech Stack</h2>
+                <h2 className="subtitle"><span class="underline-nice">Tech Stack</span></h2>
                 <div className="icon-stack-container">
-                    <img className="tech-icon" src="img/expo.png" alt="profile-img" />
-                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
-                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
-                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
-                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
-                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
+                    <img className="tech-icon" src="img/stack-icons/expo.png" alt="React Native, Expo CLI" />
+                    <img className="tech-icon" src="img/stack-icons/reactjs.png" alt="React.js" />
+                    <img className="tech-icon" src="img/stack-icons/react-native.png" alt="React.js" />
+                    <img className="tech-icon" src="img/stack-icons/js.png" alt="Vanilla js" />
+                    <img className="tech-icon" src="img/stack-icons/node-js.png" alt="Node js" />
+                    <img className="tech-icon" src="img/stack-icons/npm.png" alt="Node Package Manager - NPM" />
+                    <img className="tech-icon" src="img/stack-icons/html5.png" alt="Hypertext Markup Language - Html 5" />
+                    <img className="tech-icon" src="img/stack-icons/css3.png" alt="Stylesheet Cascades - CSS3" />
+                    <img className="tech-icon" src="img/stack-icons/sass.png" alt="Sass: Syntactically Awesome Style Sheets" />
+                    <img className="tech-icon" src="img/stack-icons/chakra.png" alt="Chakra UI" />
+
+
                 </div>
 
             </div>
