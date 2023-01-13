95a00e07660114f282ecdc2b7a7e018a8be8c550
Julian Battaglino <julianbattaglino@gmail.com>
2023-01-13 10:05:14 -0300

    home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import "./Home.css";
-import { SiGithub, SiLinkedin } from "react-icons/si";
+import { SiGithub, SiLinkedin, SiReact, SiHtml5, SiCss3, SiExpo } from "react-icons/si";
 import { ImFilePdf } from "react-icons/im";
 import Pdf from '../../cv-julian-battaglino.pdf';
 import ContactUs from "../ContactUs/ContactUs";
@@ -15,7 +15,7 @@ const Home = () => {
     return (
         <>
             <div className="intro-container">
-            
+
                 <h1 className="main-title"><span class="underline-nice">Julian Battaglino</span></h1>
                 <h2 className="subtitle">Front End Developer</h2>
                 <div className="icon-container">
@@ -28,8 +28,22 @@ const Home = () => {
                     <ImFilePdf onClick={onResumeClick} size={30} className="icon" />
                 </div>
                 <img className="profile-img" src="/img/profile-pic.png" alt="profile-img" />
+
+
+                <h2 className="subtitle">Tech Stack</h2>
+                <div className="icon-stack-container">
+                    <img className="tech-icon" src="img/expo.png" alt="profile-img" />
+                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
+                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
+                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
+                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
+                    <img className="tech-icon" src="img/reactjs.png" alt="profile-img" />
+                </div>
+
             </div>
             {/* <ContactUs /> */}
+
+
         </>
     );
 }
